f8f45cc083006a2df1d7b4211720bef5c3c27a0d
oxydo <dantunezglez@gmail.com>
2015-09-14 03:56:41 +0200

    Added de license in the Shaders

--- src/shaders/4_perspective-corrected/pass_3_normalization/vertexShader.glsl
+++ src/shaders/4_perspective-corrected/pass_3_normalization/vertexShader.glsl
@@ -1,4 +1,31 @@
 //Perspective Correct Rasterization, Gouraud Shading (Normalization Pass)
+/*
+ *
+ * CUBE
+ *
+ * Copyright (c) David Antunez Gonzalez 2013-2015 <dantunezglez@gmail.com> 
+ * Copyright (c) Luis Omar Alvarez Mures 2013-2015 <omar.alvarez@udc.es> 
+ * Copyright (c) Emilio Padron Gonzalez 2013-2015 <emilioj@gmail.com> 
+ *
+ * All rights reserved.
+ *
+ * This file is part of ToView.
+ *
+ * CUBE is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library.
+ *
+ */
+ 
 #version 410
 in  vec3 in_Position;
 in 	vec3 in_Color;
